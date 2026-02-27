ccf9a3c18340874be9ad4f8b91710a05292a6e9a
CookieHCl <nhc7502@snu.ac.kr>
2026-02-27 17:47:51 +0900

    Add more RNG library functions

--- calculation/include/RNG.h
+++ calculation/include/RNG.h
@@ -1,12 +1,14 @@
 #pragma once
-#include <random>
-#include <chrono>
-#include <atomic>
+#include <bit>
+#include <cmath>
+// Types
 #include <cstdint>
 #include <stdfloat>
-#include <bit>
+// Only for Seeder
+#include <atomic>
+#include <chrono>
+#include <random>
 
-// 64bit [0.0~1.0) RNG
 class RNG
 {
 private:
@@ -64,11 +66,138 @@ public:
 		return rand();
 	}
 
+	// unsigned integers
+	// N shoulde be N > 0
+	inline uint64_t lessThan(uint64_t N) // [0, N)
+	{
+		__uint128_t m = (__uint128_t)(rand()) * (__uint128_t)N;
+		uint64_t l = (uint64_t)m;
+
+		if (l < N) [[unlikely]]
+		{
+			uint64_t t = -N % N;
+
+			while (l < t)
+			{
+				m = (__uint128_t)(rand()) * (__uint128_t)N;
+				l = (uint64_t)m;
+			}
+		}
+
+		return m >> 64;
+	}
+	// N should be N > 0
+	template <uint64_t N>
+	inline uint64_t lessThan() // [0, N), slightly faster with constexpr
+	{
+		constexpr uint64_t t = -N % N;
+
+		__uint128_t m;
+
+		do
+		{
+			m = (__uint128_t)(rand()) * (__uint128_t)N;
+			uint64_t l = (uint64_t)m;
+		} while ((uint64_t)m < t);
+
+		return m >> 64;
+	}
+
+	// integers
+	// should be A <= B
+	inline int64_t between(int64_t A, int64_t B) // [A, B]
+	{
+		uint64_t distance = (uint64_t)B - (uint64_t)A;
+		if (distance == 0) [[unlikely]]
+		{
+			return A;
+		}
+		else if (distance == UINT64_MAX) [[unlikely]]
+		{
+			return (int64_t)(rand());
+		}
+
+		return A + (int64_t)(lessThan(distance + 1));
+	}
+	// should be A <= B
+	template <int64_t A, int64_t B>
+	inline int64_t between() // [A, B], slightly faster with constexpr
+	{
+		constexpr uint64_t distance = (uint64_t)B - (uint64_t)A;
+		if constexpr (distance == 0)
+		{
+			return A;
+		}
+		else if constexpr (distance == UINT64_MAX)
+		{
+			return (int64_t)(rand());
+		}
+
+		return A + (int64_t)(lessThan(distance + 1));
+	}
+
+	// floating-point numbers
 	inline float64_t ie01() // [0,1)
 	{
 		uint64_t rand52 = rand() >> 12;
 		uint64_t bits = 0x3FF0000000000000ULL | rand52;
 		return std::bit_cast<float64_t>(bits) - 1.0;
+	}
+	inline float64_t ei01() // (0,1]
+	{
+		uint64_t rand52 = rand() >> 12;
+		uint64_t bits = 0x3FF0000000000000ULL | rand52;
+		return 2.0 - std::bit_cast<float64_t>(bits);
+	}
+	inline float64_t ee01() // (0,1)
+	{
+		uint64_t rand52 = rand() >> 12;						// 0 ~ 2^52-1
+		return ((float64_t)rand52 + 0.5f64) * 0x1.0p-52f64; // 0.5 / 2^52 ~ (2^52-0.5) / 2^52
+	}
+	inline float64_t ii01() // [0,1]
+	{
+		uint64_t rand53 = rand() >> 11;								   // 0 ~ 2^53-1
+		return ((float64_t)rand53) * (1.0f64 / 9007199254740991.0f64); // 0 ~ (2^53-1) / (2^53-1)
+	}
+
+	// should be A < B
+	inline float64_t ieRange(float64_t A, float64_t B) // [A, B)
+	{
+		float64_t res = std::fma(ie01(), B - A, A);
+		if (res >= B) [[unlikely]]
+		{
+			res = std::nextafter(B, A);
+		}
+		return res;
+	}
+	// should be A < B
+	inline float64_t eiRange(float64_t A, float64_t B) // (A, B]
+	{
+		float64_t res = std::fma(ei01(), B - A, A);
+		if (res <= A) [[unlikely]]
+		{
+			res = std::nextafter(A, B);
+		}
+		return res;
+	}
+	// should be A < B
+	inline float64_t eeRange(float64_t A, float64_t B) // (A, B)
+	{
+		float64_t res = std::fma(ee01(), B - A, A);
+		if (res <= A) [[unlikely]]
+		{
+			res = std::nextafter(A, B);
+		}
+		else if (res >= B) [[unlikely]]
+		{
+			res = std::nextafter(B, A);
+		}
+		return res;
+	}
+	// should be A <= B
+	inline float64_t iiRange(float64_t A, float64_t B) // [A, B]
+	{
+		return std::fma(ii01(), B - A, A);
 	}
 
 	inline bool hit(float64_t probability)
